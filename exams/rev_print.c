078d4915c101c0febe949432caf5381024cc6399
beyaztakkelihacker <151129176+beyaztakkeli@users.noreply.github.com>
2024-02-05 12:41:52 +0300

    Add files via upload

---
+++ exams/rev_print.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+int ft_strlen(char *str)
+{
+	int i = 0;
+	while(str[i])
+		i++;
+	return i;
+}
+
+char *ft_rev_print(char *str)
+{
+	int i; 
+	i = ft_strlen(str);
+	i--;
+	while(i >= 0)
+	{
+		write(1, &str[i], 1);
+		i--;
+	}
+	return str;
+}
+
+int main()
+{
+	char str[] = "ghjk";
+	ft_rev_print(str);
+}
